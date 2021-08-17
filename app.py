f83a286ef39bb4545b6d356f1521798254b2687c
ben-wycliff <ben12wycliff@gmail.com>
2021-08-17 23:27:05 +0300

    Stores text in database while avoiding duplicates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING, errors
+from datetime import datetime as date
 import os
 
 env_path=os.path.join(os.getcwd(), ".env")
@@ -10,3 +11,8 @@ if os.path.exists(env_path):
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 client = MongoClient(DATABASE_URL)
+try:
+    client.gambuuze.lines.create_index([("name", ASCENDING)], unique=True)
+    client.gambuuze.lines.insert({"text": "ben", "date": date.now()})
+except errors.DuplicateKeyError:
+    print("Text already exists")
