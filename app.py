8515ed3ef3b0a5e2c18a779036299c2dd93bebb5
ben-wycliff <ben12wycliff@gmail.com>
2021-08-17 23:00:28 +0300

    Initializes mongo client

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from pymongo import MongoClient
 import os
 
 env_path=os.path.join(os.getcwd(), ".env")
@@ -7,3 +8,5 @@ if os.path.exists(env_path):
     load_dotenv(env_path)
 
 DATABASE_URL = os.getenv('DATABASE_URL')
+
+client = MongoClient(DATABASE_URL)
