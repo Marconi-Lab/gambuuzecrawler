5318d4d2e97ce6354a65607d743ba2a7fe6ded26
ben-wycliff <ben12wycliff@gmail.com>
2021-08-18 17:48:51 +0300

    Ensures that text doesn't consist of special characters

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 from pymongo import MongoClient, ASCENDING, errors
 from datetime import datetime as date
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 import os
 import requests
+import re
 
 # Import utils
 from utils import crawl
@@ -23,41 +26,59 @@ client = MongoClient(DATABASE_URL)
 def extract_text(array):
     """" Extracts text for every element in the array"""
     new_set = set()
-    for i in new_set:
+    # Special character regex
+    regex = re.compile('[_#$^*=<>©@\|}{~]')
+    for i in array:
         for j in i.find_all():
             j.decompose()
-        new_set.add(i)
+        text = i.get_text()
+        # Check if string consists Special character
+        if regex.search(text) == None:
+            new_set.add(text)
+        else:
+            continue
+        
     return new_set
 
 # Crawl site
-TARGET_URL = os.getenv('TARGET_URL')
-try:
-    all_urls = crawl(TARGET_URL)
-    client.gambuuze.lines.create_index([("text", ASCENDING)], unique=True)
+def run(url):
+    try:
+        all_urls = crawl(url)
+        # Create index key (text)
+        client.gambuuze.lines.create_index([("text", ASCENDING)], unique=True)
 
-    for link in all_urls:
-        res = requests.get(link)
-        page = BeautifulSoup(res.text, 'lxml')
-    
-        # Select all headings
-        headings = page.select(".jeg_post_title")
-        headings = extract_text(headings)
-        # Select all Paragraphs
-        paragraphs = page.select("p")
-        paragraphs = extract_text(paragraphs)
-        # Select all anchor tags
-        anchor_tags = page.select("a")
-        anchor_tags = extract_text(anchor_tags)
+        for link in all_urls:
+            session = requests.Session()
+            retry  = Retry(connect=3, backoff_factor=0.5)
+            adapter = HTTPAdapter(max_retries=retry)
+            session.mount('http://', adapter)
+            session.mount('https://', adapter)
+            res = session.get(link)
+            page = BeautifulSoup(res.text, 'lxml')
+        
+            # Select all headings
+            headings = page.select("h1")
+            headings = extract_text(headings)
+            # Select all Paragraphs
+            paragraphs = page.select("p")
+            paragraphs = extract_text(paragraphs)
+            # Select all anchor tags
+            anchor_tags = page.select("a")
+            anchor_tags = extract_text(anchor_tags)
 
-        page_data = headings.union(paragraphs,anchor_tags)
-        for text in page_data:
-            if len(text.split(" ")) < 4:
-                continue
-            else:
-                try:
-                    print(f"Writing {str(text)}")
-                    client.gambuuze.lines.insert({"text": str(text), "date": date.now()})
-                except errors.DuplicateKeyError:
-                    print("Text already exists")
-except Exception as e:
-    print(e)
+            page_data = headings.union(paragraphs,anchor_tags)
+            for text in page_data:
+                if len(text.split(" ")) < 4:
+                    continue
+                else:
+                    try:
+                        print(f"Writing {str(text)}")
+                        client.gambuuze.lines.insert({"text": str(text), "date": date.now()})
+                    except errors.DuplicateKeyError:
+                        print("Text already exists")
+    except Exception as e:
+        print(e)
+        
+if __name__ == '__main__':
+    TARGET_URL = os.getenv('TARGET_URL')
+    run(TARGET_URL)
